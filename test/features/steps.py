464984d99fc5955fb922445909e2f3dc10f9cd6d
Andreas Kring <andreas@magenta.dk>
2017-08-30 14:04:04 +0200

    Style issues

--- test/features/steps.py
+++ test/features/steps.py
@@ -3,29 +3,34 @@ import json
 from lettuce import step, world, before
 from nose.tools import assert_equals
 from app.application import app
-from app.views import USERS
+from app.routes import USERS
 
 
 @before.all
 def before_all():
     world.app = app.test_client()
 
-@step(u'Given some users are in the system')
-def given_some_users_are_in_the_system(step):
-    USERS.update({
-        'chuck': {
-            'name': 'Chuck Norris'
-        }
-    })
 
-@step(u'When I retrieve the customer \'(.*)\'')
+@step(u"Given some users are in the system")
+def given_some_users_are_in_the_system(step):
+    # USERS.update({
+    #     'chuck': {
+    #         'name': 'Chuck Norris'
+    #     }
+    # })
+    pass
+
+
+@step(u"When I retrieve the customer '(.*)'")
 def when_i_retrieve_the_customer_group1(step, username):
     world.response = world.app.get('/user/{}'.format(username))
 
-@step(u'Then I should get a \'(.*)\' response')
+
+@step(u"Then I should get a '(.*)' response")
 def then_i_should_get_a_group1_response_group2(step, expected_status_code):
     assert_equals(world.response.status_code, int(expected_status_code))
 
-@step(u'And the following user details are returned:')
+
+@step(u"And the following user details are returned:")
 def and_the_following_user_details(step):
     assert_equals(step.hashes, [json.loads(world.response.data)])
